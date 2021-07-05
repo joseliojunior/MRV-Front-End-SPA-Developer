9625e4fb7a161514105df66e53f89b3fff26b610
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-07-05 01:13:33 -0300

    Update 03. Quanta Mandioca.js

--- challenges/thirdgroup/03. Quanta Mandioca.js
+++ challenges/thirdgroup/03. Quanta Mandioca.js
@@ -3,7 +3,4 @@
  * @copyright Josélio de S. C. Júnior 2021
  */
 
-const total = [300, 1500, 600, 1000, 150]
-  .map(x => x * parseInt(gets()))
-  .reduce((a, b) => a + b) + 225;
-console.log(total);
+console.log([300, 1500, 600, 1000, 150].map(x => x * parseInt(gets())).reduce((a, b) => a + b) + 225);
