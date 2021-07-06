d0d4c86e8c216b80ee9def7055d8ee636f9a68c8
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-07-06 08:12:37 -0300

    Update 03. Análise de Números.js

--- challenges/secondgroup/03. Análise de Números.js
+++ challenges/secondgroup/03. Análise de Números.js
@@ -3,19 +3,16 @@
  * @copyright Josélio de S. C. Júnior 2021
  */
 
-const numero = [];
-for (let i = 0; i < 5; i++) {
-    numero.push(Number(gets()));
-}
+const num = [];
+while (num.length < 5) num.push(+gets());
 
-const pares = numero.filter(e => e % 2 == 0);
-const impares = numero.filter(e => e % 2 != 0);
+const even = num.filter(e => e % 2 == 0);
+const odd = num.filter(e => e % 2 != 0);
 
-const positivos = numero.filter(e => e > 0);
-const negativos = numero.filter(e => e < 0);
+const positives = num.filter(e => e > 0);
+const negatives = num.filter(e => e < 0);
 
-console.log(pares.length + " valor(es) par(es)");
-console.log(impares.length + " valor(es) impar(es)");
-
-console.log(positivos.length + " valor(es) positivo(s)");
-console.log(negativos.length + " valor(es) negativo(s)");
+console.log(`${even.length} valor(es) par(es)`);
+console.log(`${odd.length} valor(es) impar(es)`);
+console.log(`${positives.length} valor(es) positivo(s)`);
+console.log(`${negatives.length} valor(es) negativo(s)`);
