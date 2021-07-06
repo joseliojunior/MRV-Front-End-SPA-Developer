b4f87b183e2d747f3bc83d308f1755b44fadf5d3
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-07-06 08:16:59 -0300

    Update 02. Folha de Pagamento.js

--- challenges/thirdgroup/02. Folha de Pagamento.js
+++ challenges/thirdgroup/02. Folha de Pagamento.js
@@ -3,9 +3,7 @@
  * @copyright Josélio de S. C. Júnior 2021
  */
 
-const v1 = parseInt(gets());
-const v2 = parseInt(gets());
-const v3 = parseFloat(gets());
-const salary = parseFloat(v2 * v3).toFixed(2);
+const v1 = +gets(), v2 = +gets(), v3 = +gets(),
+salary = (v2 * v3).toFixed(2);
 console.log(`NUMBER = ${v1}`);
 console.log(`SALARY = U$ ${salary}`);
