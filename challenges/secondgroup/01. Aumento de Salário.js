272284129587ca9c5d1dbbebabd59d6085f9a139
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-07-13 02:56:03 -0300

    Update 01. Aumento de Salário.js

--- challenges/secondgroup/01. Aumento de Salário.js
+++ challenges/secondgroup/01. Aumento de Salário.js
@@ -6,17 +6,11 @@
 const salary = +gets();
 let percent;
 
-if (salary <= 400 ) {
-  percent = 15;
-} else if (salary >= 400.01 && salary <= 800) {
-  percent = 12;
-} else if (salary >= 800.01 && salary <= 1200) {
-  percent = 10;
-} else if (salary >= 1200.01 && salary <= 2000) {
-  percent = 7;
-} else {
-  percent = 4;
-}
+percent = salary <= 400 ? 15
+  : salary >= 400.01 && salary <= 800 ? 12
+    : salary >= 800.01 && salary <= 1200 ? 10
+      : salary >= 1200.01 && salary <= 2000 ? 7
+        : 4;
 
 const reajust = salary * (percent / 100);
 const newSalary = salary + reajust;
