2f8fa258891fa328d090d00629200427659587fb
Josélio Júnior <76992016+joseliojunior@users.noreply.github.com>
2021-08-25 17:46:38 -0300

    new fix

--- challenges/thirdgroup/01. Área Superior.js
+++ challenges/thirdgroup/01. Área Superior.js
@@ -3,23 +3,20 @@
  * @copyright Josélio de S. C. Júnior 2021
  */
 
-const operator = gets();
-const fl = gets();
-let x = 1, y = 11, arr = [];
+const nums = x => x?.trim().split(/[^-0-9.]+/g);
+let aX = [gets(), ...nums(gets())];
+const getsFixed = _ => (aX = aX.length === 0 ? nums(gets()) : aX, aX?.shift());
 
-if (fl.match(/\s/g)) {
-    const fn = x => x.replace(/\s+/g, ' ').trim().split(' ').map(e => +e);
+const operator = getsFixed();
+let x = 1, y = 11;
 
-    arr.push(...[fn(fl), ...Array.from(Array(4), () => fn(gets()))])
-} else {
-    arr.push([fl, ...Array.from(Array(11), _ => +gets())],
-        ...Array.from(Array(4), _ =>
-            Array.from(Array(12), _ => +gets()))
-    );
-};
+const arr = Array.from(Array(5), _ =>
+    Array.from(Array(12), _ => +getsFixed()))
+    .map(e => (e = e.slice(x, y), x++, y--, e)
+    ).flat();
 
-const sum = arr.flatMap(e => (e = e.slice(x, y), x++, y--, e))
-    .reduce((a, b) => a + b);
+let total = arr.reduce((a, b) => a + b);
 
-const total = operator === 'M' ? sum / 30 : sum;
+operator === 'M' ? total /= arr.length : total;
+
 console.log(total.toFixed(1));
